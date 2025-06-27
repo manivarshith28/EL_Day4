9693afa11a4452ba29adf6e56bc39b4bd34cf98a
manivarshith28 <manivarshith2806@gmail.com>
2025-06-27 21:51:45 +0530

    inserting sample data into table

---
+++ insertion.sql
@@ -0,0 +1,21 @@
+INSERT INTO indian_cricketer_stats
+(player_id, name, age, matches_played, runs_scored, wickets_taken, role)
+VALUES
+(1, 'Virat Kohli', 36, 500, 24000, 4, 'Batter'),
+(2, 'Rohit Sharma', 37, 470, 19000, 1, 'Batter'),
+(3, 'Hardik Pandya', 31, 150, 4500, 100, 'All-Rounder'),
+(4, 'Ravindra Jadeja', 36, 310, 5800, 250, 'All-Rounder'),
+(5, 'Jasprit Bumrah', 31, 200, 100, 350, 'Bowler'),
+(6, 'Shubman Gill', 25, 90, 3600, 0, 'Batter'),
+(7, 'Kuldeep Yadav', 30, 120, 50, 180, 'Bowler'),
+(8, 'Rishabh Pant', 27, 130, 5100, 1, 'WK-Batter'),
+(9, 'KL Rahul', 32, 160, 6200, 2, 'WK-Batter'),
+(10, 'Mohammed Siraj', 31, 110, 60, 170, 'Bowler'),
+(11, 'Sanju Samson', 30, 85, 3000, 0, 'WK-Batter'),
+(12, 'Axar Patel', 31, 100, 1500, 140, 'All-Rounder'),
+(13, 'Prithvi Shaw', 25, 60, 2000, 0, 'Batter'),
+(14, 'Yuzvendra Chahal', 34, 120, 120, 190, 'Bowler'),
+(15, 'Washington Sundar', 26, 70, 1100, 70, 'All-Rounder'),
+(16, 'Ishan Kishan', 27, 75, 2500, 0, 'WK-Batter'),
+(17, 'Bhuvneshwar Kumar', 34, 250, 500, 280, 'Bowler'),
+(18, 'Ruturaj Gaikwad', 28, 60, 2200, 0, 'Batter');
